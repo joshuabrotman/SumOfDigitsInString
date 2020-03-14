1289036c930c0c32c9a092fd6175097472d5a97f
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 15:26:44 -0500

    message

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int totalDigits = 0;
+            
+            Console.WriteLine("Please enter a string containing digits:");
+            string userInput = Console.ReadLine();
+
+            
+
+            foreach (char i in userInput)
+            {
+                if (Regex.IsMatch(i.ToString(), @"^\d+$")) { totalDigits += int.Parse(i.ToString()); }
+                
+            }
+
+            Console.WriteLine("The sum is:" + totalDigits);
+            
+            Console.ReadKey();
+        }
+    }
+}
